b3e6db0ef751e26f391da4a9aa063501e2d85cab
Ewan Redfield <29620734+chryoung@users.noreply.github.com>
2024-09-02 01:33:55 +0800

    Add navigation to mod page

--- Cyberpunk2077SaveModManager/MainWindow.xaml.cs
+++ Cyberpunk2077SaveModManager/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
 
 namespace Cyberpunk2077SaveModManager
 {
@@ -23,7 +25,65 @@ namespace Cyberpunk2077SaveModManager
         public MainWindow()
         {
             this.InitializeComponent();
-            this.MainFrame.Navigate(typeof(SaveManager));
+        }
+
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.NavView.SelectedItem = this.NavView.MenuItems[0];
+            this.NavView_Navigate(typeof(SaveManager), new EntranceNavigationTransitionInfo());
+        }
+
+        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked == true)
+            {
+                NavView_Navigate(typeof(SettingsPage), args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.InvokedItemContainer != null)
+            {
+                Type navPageType = Type.GetType(args.InvokedItemContainer.Tag.ToString());
+                NavView_Navigate(navPageType, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            this.TryGoBack();
+        }
+
+        private bool TryGoBack()
+        {
+            if (!this.ContentFrame.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (this.NavView.IsPaneOpen &&
+                (this.NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 this.NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            this.ContentFrame.GoBack();
+            return true;
+        }
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void NavView_Navigate(
+            Type navPageType,
+            NavigationTransitionInfo transitionInfo)
+        {
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            Type preNavPageType = this.ContentFrame.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (navPageType is not null && !Type.Equals(preNavPageType, navPageType))
+            {
+                this.ContentFrame.Navigate(navPageType, null, transitionInfo);
+            }
         }
     }
 }
